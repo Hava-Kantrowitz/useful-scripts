96093535a55f2ca1bef6808dbdc108f934f5f3aa
Hava-Kantrowitz <46357722+Hava-Kantrowitz@users.noreply.github.com>
2022-01-23 17:26:31 -0500

    Create change-all-passwords.ps1

---
+++ change-all-passwords.ps1
@@ -0,0 +1,7 @@
+#Changes all domain passwords for Linux at once
+
+Import-Module ActiveDirectory
+$set = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray()
+$allusers = Get-ADUser -Properties memberof -Filter *
+$non_DA_users = foreach ($user in $allusers) {if ($user.MemberOf -join ';' -notmatch 'Domain Admins' -and $user.name -notmatch 'krbtgt') {Write-Output $user.name}}
+foreach ($user in $non_DA_users) {$password="";for ($i=1; $i -le 64; $i++){$password += $set | Get-Random};net user $user $password}
